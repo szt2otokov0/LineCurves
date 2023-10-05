ffc5ec7e2489c73cdd483d5397a5ca3a5b45899d
szt2otokov <szt2otokov@vasvari.org>
2023-10-05 13:26:17 +0200

    it draws one curve, I couldn't make the other one tho

---
+++ src/DrawRectangle.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class DrawRectangle {
+
+    // ezt a részt nem kell módosítani
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("I'm here...");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        DrawRectangle.ImagePanel panel = new DrawRectangle.ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    public static void drawRect(Graphics gfx){
+        drawLineCurve(gfx, 0, 500);
+        //drawLineCurve(gfx,500,-500);  //fixme:this should mirror it, but it doesn't :(
+    }
+
+    private static void drawLineCurve(Graphics gfx, int x, int y) {
+        if(y < 0) {
+            for(int i = y;i < 0;i += 10){
+                gfx.drawLine(x+i,(int)Math.round(Math.log10(i)),x,y+i);
+            }
+        } else {
+            for(int i = 0;i < y;i += 10){
+                gfx.drawLine(x+i,(int)Math.round(Math.log10(i)),x,y-i);
+            }
+        }
+
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            drawRect(graphics);
+        }
+    }
+}
